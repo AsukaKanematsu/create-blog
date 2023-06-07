e507671a7cd14e47edb7b348f63068708b475611
KatsuyaTanaka95 <gu0520kt@gmail.com>
2023-06-07 15:16:49 +0900

    fix: ブログの作成機能を修正しました。

--- src/public/post/store.php
+++ src/public/post/store.php
@@ -7,19 +7,23 @@ $pdo = new PDO(
     $dbPassword
 );
 
+//ログイン機能を作成していないため、仮で作成しています。
+$userId = 1;
 $contents = filter_input(INPUT_POST, 'contents');
 $title = filter_input(INPUT_POST, 'title');
 
 // [解説！]ガード節になっている
 if (!empty($title) && !empty($contents)) {
-    $sql = 'INSERT INTO `blogs`(`title`, `contents`) VALUES(:title, :contents)';
+    $sql =
+        'INSERT INTO `blogs`(`user_id`, `title`, `contents`) VALUES(:userId, :title, :contents)';
     $statement = $pdo->prepare($sql);
+    $statement->bindValue(':userId', $userId, PDO::PARAM_INT);
     $statement->bindValue(':title', $title, PDO::PARAM_STR);
     $statement->bindValue(':contents', $contents, PDO::PARAM_STR);
     $statement->execute();
 
     // [解説！]リダイレクト処理
-    header('Location: ./create.php');
+    header('Location: ../create.php');
     // [解説！]リダイレクトしても処理が一番下まで続いてしまうので「exit」しておこう！！！
     exit();
 }
@@ -29,8 +33,8 @@ $error = 'タイトルまたは本文が入力されていません';
 <body>
   <div>
     <p><?php echo $error . "\n"; ?></p>
-    <a href="./index.php">
-        <p>トップページへ</p>
+    <a href="../index.php">
+      <p>トップページへ</p>
     </a>
   </div>
 </body>
